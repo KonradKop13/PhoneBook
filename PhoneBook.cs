1802d2a30f304e58cc6a0ab74c15baace67210a6
Asus <Asus@LAPTOP-9QENFRBF>
2023-12-21 17:10:34 +0100

    Add project files.

---
+++ PhoneBook.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    internal class PhoneBook
+    {
+        public List<Contact> Contacts { get; set; } = new List<Contact>();
+        public void AddContact(Contact contact)
+        {
+            if(contact.Number.Length==9) {
+                Contacts.Add(contact);
+            }
+            else {
+                Console.WriteLine("You have entered an invalid number");
+            }
+            
+        }
+        private void DisplayContactDetails(Contact contact)
+        {
+            Console.WriteLine($"Contact: {contact.Name} , {contact.Number} ");
+        }
+        private void DisplayContactsDetails(List<Contact> contacts)
+        {
+            foreach (Contact contact in contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void DisplayContact(string number)
+        {
+            Contact contact = Contacts.FirstOrDefault(c => c.Number == number);
+            if (contact == null)
+            {
+                Console.WriteLine("Contact is not found");
+            }
+            else
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+        public void DisplayAllContacts()
+        {
+            DisplayContactsDetails(Contacts);
+        }
+
+        public void DisplayMatchingContacts(string searched)
+        {
+            var matchingContacts = Contacts.Where(c => c.Name.Contains(searched)).ToList();
+            DisplayContactsDetails(matchingContacts); 
+        }
+
+       
+        public void DeleteContact(string name)
+        {
+            var toDeleteName = Contacts.FirstOrDefault(c => c.Name == name);
+            Contacts.Remove(toDeleteName);
+
+        }
+        public void Edit(string name, string number)
+        {
+            foreach(Contact contact in Contacts) {
+                if(contact.Name == name)
+                {
+                    var usersName = Contacts.FirstOrDefault(c => c.Name == name);
+                    if (number.Length == 9)
+                    {
+                        usersName.Number = number;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You have entered an invalid phone number");
+                    }
+                }
+                else { Console.WriteLine("The username you entered does not exist"); }
+                
+            }
+            
+           
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿namespace PhoneBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello in PhonEBook app");
+            Console.WriteLine("1 Add contact");
+            Console.WriteLine("2 Display contact by number ");
+            Console.WriteLine("3 Display all contacts");
+            Console.WriteLine("4 Search contacts");
+            Console.WriteLine("5 Delete Item After User Name");
+            Console.WriteLine("6 Edit a user's phone number");
+            Console.WriteLine("To exit insert 'end'");
+
+            var userInput = Console.ReadLine();
+            var phoneBook = new PhoneBook();
+            while (true) {
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Insert number(it must be 9 digits long)");
+                        var number = Console.ReadLine();
+                        Console.WriteLine("Insert name");
+                        var name = Console.ReadLine();
+                        var newContact = new Contact(name, number);
+                        phoneBook.AddContact(newContact);
+
+                        break;
+                    case "2":
+                        Console.WriteLine("Insert number");
+                        var numberToSearch = Console.ReadLine();
+                        phoneBook.DisplayContact(numberToSearch);
+                        break;
+                    case "3":
+                        phoneBook.DisplayAllContacts();
+                        break;
+                    case "4":
+                        Console.WriteLine("Insert search phrase");
+                        var searchPhrase = Console.ReadLine();
+                        phoneBook.DisplayMatchingContacts(searchPhrase);
+                        break;
+                     
+                    case "5":
+                        Console.WriteLine("Enter the name of the person you want to remove");
+                        var toDeleteName = Console.ReadLine();
+                        phoneBook.DeleteContact(toDeleteName);
+
+                        break;
+                    case "6":
+                        Console.WriteLine("Podaj imie urzytkownika którego numer chcesz zmienić");
+                        var userName = Console.ReadLine();
+                        Console.WriteLine("podaj numer który chcesz zapisać do tego urzytkownika");
+                        var userNumber = Console.ReadLine();
+                        phoneBook.Edit(userName, userNumber);
+                        break;
+                    case "end":
+                        return;
+                     default:
+                        Console.WriteLine("Invalid operation");
+                        break;
+                }
+                Console.WriteLine("Select operation");
+                 userInput = Console.ReadLine();
+            }
+        }
+    }
+}
